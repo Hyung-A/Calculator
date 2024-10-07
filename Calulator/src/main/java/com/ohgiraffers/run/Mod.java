9def3aeae7b16a641042b1f8cc861204957075b5
i7E-88 <gudjtr097@gmail.com>
2024-10-07 17:33:30 +0900

    10/07

--- Calulator/src/main/java/com/ohgiraffers/run/Mod.java
+++ Calulator/src/main/java/com/ohgiraffers/run/Mod.java
@@ -1,21 +1,22 @@
 package com.ohgiraffers.run;
 
-public class Divide {
+public class Mod {
 
-
-    public int divideFunction(int a, int b){
+    public int modFunction(int a, int b){
         int result = 0;
 
-        try {
-            result = Math.round(a/b);
+
+//        round는 반올림
+        try{
+            result = a%b;
         }catch (IllegalArgumentException e){
             throw new IllegalArgumentException();
         }
-        catch (ArithmeticException e){
+        catch (ArithmeticException e) {
             System.out.println("0으로 나눌 수 없습니다.");
             throw new ArithmeticException();
         }
-
         return result;
     }
+
 }
