beb3774f8e4dde4181dc0b8b3d9cb37c4fc46b83
gkdsm <dlgkdms@gmail.com>
2024-10-08 17:41:05 +0900

    2024.10.08

---
+++ Calulator/src/main/java/com/ohgiraffers/run/Minus.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.run;
+
+import java.util.Scanner;
+
+public class Minus {
+
+    public int minusFunction(int a, int b) {
+
+        int result = a-b;
+
+        return result;
+
+    }
+}
